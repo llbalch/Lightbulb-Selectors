c63f9497ece276d72a3bd81dc9be21c51c45f22e
Lauren Balch <llbalch@gmail.com>
2025-06-10 13:59:29 -0500

    Updated Code

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Write your code here
+// Access the lightbult elements and store in individual variables
+const lightbulb1 = document.querySelector("#lightbulb1");
+const lightbulb2 = document.querySelector("#lightbulb2");   
+const lightbulb3 = document.querySelector("#lightbulb3");
+let subtitle = document.querySelector(".subtitle");
+let count = 0;
+
+// add event listener to the lightbulb elements
+lightbulb1.addEventListener("click", function(){
+    count ++;
+    subtitle.innerHTML = `You've clicked the lights ${count} times`;
+    this.classList.toggle("active");
+})
+
+lightbulb2.addEventListener("click", function(){
+    count ++;
+    subtitle.innerHTML = `You've clicked the lights ${count} times`;
+    this.classList.toggle("active");
+})
+
+lightbulb3.addEventListener("click", function(){
+    count ++;
+    subtitle.innerHTML = `You've clicked the lights ${count} times`;
+    this.classList.toggle("active");
+})
